9f3e0ae84ef3430c1770d36fca9052a2289b27e7
Jens Wilke <jw_github@headissue.com>
2020-08-11 08:12:39 +1000

    assert that log was created

--- log4j-test/src/test/java/SimpleTest.java
+++ log4j-test/src/test/java/SimpleTest.java
@@ -2,12 +2,8 @@ import org.cache2k.Cache;
 import org.cache2k.Cache2kBuilder;
 import static org.junit.Assert.*;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.Closeable;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.LogManager;
 
 /**
  * @author Jens Wilke
@@ -18,6 +14,7 @@ public class SimpleTest {
     System.setProperty("log4j2.contextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
   }
 
+  /*
   void checkThreads() {
     System.err.println("THREADS");
     for (Thread t : Thread.getAllStackTraces().keySet()) {
@@ -25,10 +22,10 @@ public class SimpleTest {
     }
     System.err.println(ThreadDump.generateThreadDump());
   }
+  */
 
   @Test
   public void testCache() {
-    checkThreads();
     Cache<String,String> c = new Cache2kBuilder<String, String>() {}
       .expireAfterWrite(5, TimeUnit.MINUTES)
       .resilienceDuration(30, TimeUnit.SECONDS)
@@ -37,37 +34,12 @@ public class SimpleTest {
       .build();
     assertEquals("3", c.get("abc"));
     c.close();
-    checkThreads();
+    assertTrue("Log framework created the debug logger",
+      org.apache.logging.log4j.LogManager.getContext().hasLogger("org.cache2k.core.util.Log"));
   }
 
-  String expensiveOperation(String s) {
+  private String expensiveOperation(String s) {
     return s.length() + "";
-  }
-
-  @Test
-  public void logTest1() {
-    checkThreads();
-    Logger logger = LoggerFactory.getLogger("log1");
-    checkThreads();
-  }
-
-  @Test
-  public void logTest2() {
-    checkThreads();
-    org.apache.logging.log4j.Logger logger =
-      org.apache.logging.log4j.LogManager.getLogger("log2");
-    checkThreads();
-
-
-  }
-
-  @Test
-  public void logTest3() throws Exception {
-    checkThreads();
-    org.apache.logging.log4j.Logger logger =
-      org.apache.logging.log4j.LogManager.getLogger("log3");
-    logger.debug("hello");
-    checkThreads();
   }
 
 }
