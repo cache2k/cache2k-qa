08d59eb2d078bfd603e0cce88104054390d59c3d
Jens Wilke <jw_github@headissue.com>
2020-08-25 14:55:00 +1000

    provide a cache manager

--- spring-boot-jcache-test/src/main/java/sample/cache/CacheConfiguration.java
+++ spring-boot-jcache-test/src/main/java/sample/cache/CacheConfiguration.java
@@ -1,11 +1,20 @@
 package sample.cache;
 
 import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
+import org.springframework.cache.Cache;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.jcache.JCacheCache;
+import org.springframework.cache.jcache.JCacheCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.cache.configuration.MutableConfiguration;
+import javax.cache.expiry.Duration;
+import javax.cache.expiry.EternalExpiryPolicy;
+import javax.cache.expiry.ModifiedExpiryPolicy;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Jens Wilke
@@ -14,6 +23,30 @@ import javax.cache.configuration.MutableConfiguration;
 @EnableCaching
 public class CacheConfiguration {
 
+  /**
+   * Standard Spring
+   */
+  /*-
+  @Bean
+  public JCacheCacheManager cacheManager() {
+    return new JCacheCacheManager() {
+      @Override
+      protected Collection<Cache> loadCaches() {
+        Collection<Cache> caches = new ArrayList<>();
+        caches.add(new JCacheCache(
+          getCacheManager().createCache(CountryRepository.CACHE_NAME,
+          new MutableConfiguration()
+            .setExpiryPolicyFactory(ModifiedExpiryPolicy.factoryOf(new Duration(TimeUnit.MINUTES, 30)))),
+          false));
+        return caches;
+      }
+    };
+  }
+  -*/
+
+  /**
+   * Spring Boot
+   */
   @Bean
   public JCacheManagerCustomizer cacheConfigurationCustomizer() {
     return cm -> {
